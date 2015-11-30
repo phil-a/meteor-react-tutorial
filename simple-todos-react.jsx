18a54930c4293eb067f220b34cd3773f71efaf24
phil-a <phil.alekseev@gmail.com>
2015-11-29 20:37:53 -0500

    Adding user accounts

--- simple-todos-react.jsx
+++ simple-todos-react.jsx
@@ -3,7 +3,11 @@ Tasks = new Mongo.Collection("tasks");
 
 if (Meteor.isClient) {
   // This code is executed on the client only
- 
+  //Use usernames instead of email addresses
+  Accounts.ui.config({
+    passwordSignupFields: "USERNAME_ONLY"
+  });
+
   Meteor.startup(function () {
     // Use Meteor.startup to render the component after the page is ready
     React.render(<App />, document.getElementById("render-target"));
